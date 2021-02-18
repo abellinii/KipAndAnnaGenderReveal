0f49f511f6e4aefd8e6478aba2f6010326bcd978
Wade Abel <thepassivetrust@gmail.com>
2021-02-17 19:50:52 -0800

    Updates

---
+++ precache-manifest.9e762a973ee01ae24116a02e89aa6c1c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/GenderReveal/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "c959e2d8b43b2cb51678",
+    "url": "/GenderReveal/static/js/app.fc519b2b.chunk.js"
+  },
+  {
+    "revision": "99d60b3418c710747abc",
+    "url": "/GenderReveal/static/js/runtime~app.b45a331b.js"
+  },
+  {
+    "revision": "ae2acce108126e1ee084549758523d0e",
+    "url": "/GenderReveal/static/media/babyboy.2e6d110b.png"
+  },
+  {
+    "revision": "a991b2426983e2388a398cb1d47cbc45",
+    "url": "/GenderReveal/static/media/gender.b04cc6f7.jpg"
+  },
+  {
+    "revision": "bf32354412301c3b1c2fba5cd101d343",
+    "url": "/GenderReveal/static/media/pink.d448e7bb.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/GenderReveal/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "e5bcde4037f98831490b8ae0a5e5e159",
+    "url": "/GenderReveal/manifest.json"
+  },
+  {
+    "revision": "cbd4415106446b365559041a040e37d9",
+    "url": "/GenderReveal/static/js/2.081fb13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365abc3a927496a82f62840c145cbee3",
+    "url": "/GenderReveal/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/GenderReveal/serve.json"
+  },
+  {
+    "revision": "9a94ce498fec54648b07",
+    "url": "/GenderReveal/static/js/2.081fb13a.chunk.js"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/GenderReveal/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/GenderReveal/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "17ec3e9614bd48f9f99a2b5f9f632f54",
+    "url": "/GenderReveal/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/GenderReveal/favicon-32.png"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/GenderReveal/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/GenderReveal/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/GenderReveal/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "9f35f59c29053eca9ad1182b67afb90a",
+    "url": "/GenderReveal/index.html"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/GenderReveal/favicon-16.png"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/GenderReveal/expo-service-worker.js"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/GenderReveal/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/GenderReveal/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/GenderReveal/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GenderReveal/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/GenderReveal/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/GenderReveal/./fonts/AntDesign.ttf"
+  }
+];
